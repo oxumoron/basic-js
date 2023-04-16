0953ba4aeea71248d1bf3f5242aaf4c107991f6c
oxumoron <oxymoron.dr@gmail.com>
2023-04-16 18:37:11 +0200

    feat: add 7 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let counter = 0;
+  s1 = Array.from(s1);
+  s2 = Array.from(s2);
+
+  s1.forEach((el) => {
+    if (s2.includes(el)) {
+      counter++;
+
+      s2.splice(s2.indexOf(el), 1);
+    }
+  });
+
+  return counter;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let numToArray = String(n)
+    .split("")
+    .map((el) => +el);
+
+  return Math.max(
+    ...numToArray.map((_, i) => {
+      let dig = numToArray.slice();
+      dig.splice(i, 1);
+      return Number(dig.join(""));
+    })
+  );
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
